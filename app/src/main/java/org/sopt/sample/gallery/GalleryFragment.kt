ed0880f9256d9b08527b3d704bbd66992e1d0206
Skylar <twinkle991@naver.com>
2022-12-02 09:42:59 +0900

    4차 과제 완료, 7차 세미나 실습
    GalleryFragment에 photoPicker - PickVisualMedia

--- app/src/main/java/org/sopt/sample/gallery/GalleryFragment.kt
+++ app/src/main/java/org/sopt/sample/gallery/GalleryFragment.kt
@@ -1,20 +1,58 @@
 package org.sopt.sample.gallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import coil.load
 import org.sopt.sample.R
+import org.sopt.sample.databinding.FragmentGalleryBinding
 
 class GalleryFragment: Fragment() {
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding: FragmentGalleryBinding
+        get() = checkNotNull(_binding)
+
+    /* 7주차 세미나
+    private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        binding.imageView.load(it)
+    }
+    */
+
+    // Registers a photo picker activity launcher in single-select mode.
+    val pickMedia = registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(3)) { uri ->
+        // Callback is invoked after the user selects a media item or closes the
+        // photo picker.
+        if (uri != null) {
+            Log.d("PhotoPicker", "Selected URI: $uri")
+            binding.imageView.load(uri[0])
+            binding.imageView2.load(uri[1])
+        } else {
+            Log.d("PhotoPicker", "No media selected")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imageView.setOnClickListener {
+            //launcher.launch("image/*")
+
+            // Launch the photo picker and allow the user to choose images and videos.
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))
+        }
     }
 
 }
